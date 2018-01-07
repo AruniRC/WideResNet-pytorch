ea463b97b266426c5b6d3e0b76fa0c824ee6df57
Aruni RC <arunirc@gmail.com>
2018-01-07 12:32:02 -0500

    multi-gpu wide resnet training

--- train.py
+++ train.py
@@ -106,7 +106,8 @@ def main():
     # for training on multiple GPUs.
     # Use CUDA_VISIBLE_DEVICES=0,1 to specify which GPUs to use
     # model = torch.nn.DataParallel(model).cuda()
-    model = model.cuda()
+    model = torch.nn.DataParallel(model, device_ids=[0, 1, 2, 3]).cuda()
+    # model = model.cuda()
 
     # optionally resume from a checkpoint
     if args.resume:
